f6c2fbbbac48efa551ded1ae2f2d1ef6e96aecdf
cocacolaboxers <jtomasdiaz.98@icloud.com>
2018-04-10 00:10:36 -0400

    Fixed resourceManager

---
+++ BoilerPlate/resourceManager.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef _RESOURCE_MANAGER_HPP_
+#define _RESOURCE_MANAGER_HPP_
+
+class resourceManager
+{
+public:
+	resourceManager();
+
+private:
+
+};
+#endif // !_RESOURCE_MANAGER_HPP_
